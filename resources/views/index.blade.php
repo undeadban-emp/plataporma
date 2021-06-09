f509184e9ee92e72d66bf57edaf3cdc18cdfa78a
eDcel <undeadban.emp@gmail.com>
2021-06-09 15:37:00 +0800

    redesign image and add new favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,14 +11,14 @@
 <div class="candidates" id="kilalanin">
     <div class="row">
     <div class="col-12">
-    <div class="candidate w-270 clg-hidden"><a href=""><img src="assets/images/ayek.png" class="lg-hidden" alt="Romy Macalintal" title="Romy Macalintal"></a></div>
-    <div class="candidate w-325 cxs-hidden"><a href=""><img src="assets/images/ayek.png" alt="Hon. Alexander Pimentel" title="Hon. Alexander Pimentel"></a></div>
+    <div class="candidate w-270 clg-hidden"><a href=""><img src="{{ asset('assets/images/ayek.png') }}" class="lg-hidden" alt="Hon. Alexander Pimentel" title="Hon. Alexander Pimentel"></a></div>
+    <div class="candidate w-325 cxs-hidden"><a href=""><img src="{{ asset('assets/images/ayek.png') }}" alt="Hon. Alexander Pimentel" title="Hon. Alexander Pimentel"></a></div>
     <div class="clear"></div>
     </div>
     </div>
     <div class="clear"></div>
-    <div class="candidate w-224 m-negmargin-5"><a href=""><img src="assets/images/momo.png" alt="Hon.Romeo S. Momo" title="Hon.Romeo S. Momo"></a></div>
-    <div class="candidate w-224 "><a href=""><img src="assets/images/johny.png" alt="Hon. Johnny T. Pimentel" title="Hon. Johnny T. Pimentel"></a></div>
+    <div class="candidate w-224 m-negmargin-5"><a href=""><img src="{{ asset('assets/images/momo.png') }}" alt="Hon.Romeo S. Momo" title="Hon.Romeo S. Momo"></a></div>
+    <div class="candidate w-224 "><a href=""><img src="{{ asset('assets/images/johny.png') }}" alt="Hon. Johnny T. Pimentel" title="Hon. Johnny T. Pimentel"></a></div>
     <div class="clear"></div>
     </div>
 
