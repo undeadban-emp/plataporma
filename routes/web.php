462e730e371e1d37e76ef1464989ea38c5758810
eDcel <undeadban.emp@gmail.com>
2021-06-11 13:44:03 +0800

    added plataporma page

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+Route::resource('/plataporma', 'platapormaController');
 
 Route::resource('/alexander-t-pimentel', 'alexanderController');
 Route::resource('/romeo-s-momo', 'RomeoMomoController');
